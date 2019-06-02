8bc1b3940214a19b64e8bd965c68e9393be7956d
David Johnson <djohnson.m@gmail.com>
2019-06-01 23:58:57 -0400

    mean, meanWeighted

--- src/ArrayFire/Statistics.hs
+++ src/ArrayFire/Statistics.hs
@@ -1,15 +1,32 @@
+{-# LANGUAGE ViewPatterns #-}
 module ArrayFire.Statistics where
 
 import Foreign.Marshal
 import Foreign.Storable
 import Foreign.C.String
 
--- import ArrayFire.Internal.Util
 import ArrayFire.Internal.Statistics
-import ArrayFire.Exception
 import ArrayFire.Internal.Defines
 
-mean arr dimt = do
-  alloca $ \ptr -> do
-    print =<< af_mean ptr arr dimt
-    peek ptr
+import ArrayFire.Exception
+import ArrayFire.Types
+import ArrayFire.FFI
+
+mean
+  :: AFType a
+  => Array a
+  -> Int -- * dimension
+  -> Array a
+mean x n =
+  x `op1` (\x y ->
+    af_mean x y (fromIntegral n))
+
+meanWeighted
+  :: AFType a
+  => Array a
+  -> Array a
+  -> Int -- * dimension
+  -> Array a
+meanWeighted x y (fromIntegral -> n) =
+  op2 x y $ \a b c ->
+    af_mean_weighted a b c n
