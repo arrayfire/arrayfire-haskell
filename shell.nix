1e045cc73330f46aad856e61f41dab1af1ab9efb
David Johnson <djohnson.m@gmail.com>
2019-10-13 12:09:52 -0400

    long live cabal-v1 commands.

--- shell.nix
+++ shell.nix
@@ -5,12 +5,13 @@ in
   pkgs.lib.overrideDerivation pkg (drv: {
     shellHook = ''
       function ghcid () {
-        ${pkgs.haskellPackages.ghcid}/bin/ghcid -c 'cabal repl lib:arrayfire'
+        ${pkgs.haskellPackages.ghcid}/bin/ghcid -c 'cabal v1-repl lib:arrayfire'
       };
       function test-runner () {
          ${pkgs.ag}/bin/ag -l | \
            ${pkgs.entr}/bin/entr sh -c \
-             'cabal build test && dist/build/test/test'
+             'cabal v1-configure --enable-tests && \
+                cabal v1-build test && dist/build/test/test'
       }
     '';
   })
