8689f5401c6c07509f459ed383df858dd23bfeff
David Johnson <djohnson.m@gmail.com>
2019-10-26 10:38:07 -0400

    Update features tests and docs.

--- src/ArrayFire/Features.hs
+++ src/ArrayFire/Features.hs
@@ -22,6 +22,9 @@ import ArrayFire.FFI
 import ArrayFire.Exception
 
 -- | Construct 'Features'
+--
+-- >>> features = createFeatures (createFeatures 10)
+--
 createFeatures
   :: Int
   -> Features
@@ -35,42 +38,81 @@ createFeatures (fromIntegral -> n) =
     pure (Features fptr)
 
 -- | Retain 'Features'
+--
+-- >>> features = retainFeatures (createFeatures 10)
+--
 retainFeatures
   :: Features
   -> Features
 retainFeatures = (`op1f` af_retain_features)
 
 -- | Get number of 'Feature's
+--
+-- link
+--
+-- >>> getFeaturesNum (createFeatures 10)
+-- 10
+--
 getFeaturesNum
   :: Features
   -> Int
 getFeaturesNum = fromIntegral . (`infoFromFeatures` af_get_features_num)
 
 -- | Get 'Feature' X-position
+--
+-- >>> getFeaturesXPos (createFeatures 10)
+-- ArrayFire Array
+-- [10 1 1 1]
+--    0.0000     0.0000     0.0000     0.0000     0.0000     2.1250     0.0000     2.2500     0.0000     0.0000
+--
 getFeaturesXPos
   :: Features
   -> Array a
 getFeaturesXPos = (`featuresToArray` af_get_features_xpos)
 
 -- | Get 'Feature' Y-position
+--
+-- >>> getFeaturesYPos (createFeatures 10)
+-- ArrayFire Array
+-- [10 1 1 1]
+--    0.0000     0.0000     0.0000     0.0000     0.0000     0.0000     0.0000     0.0000     0.0000     0.0000
+--
 getFeaturesYPos
   :: Features
   -> Array a
 getFeaturesYPos = (`featuresToArray` af_get_features_ypos)
 
 -- | Get 'Feature' Score
+--
+-- >>> getFeaturesScore (createFeatures 10)
+-- ArrayFire Array
+-- [10 1 1 1]
+--    0.0000     1.8750     0.0000     2.0000     0.0000     2.1250     0.0000     2.2500        nan        nan
+--
 getFeaturesScore
   :: Features
   -> Array a
 getFeaturesScore = (`featuresToArray` af_get_features_score)
 
 -- | Get 'Feature' orientation
+--
+-- >>> getFeaturesOrientation (createFeatures 10)
+-- ArrayFire Array
+-- [10 1 1 1]
+--    0.0000     1.8750     0.0000     2.0000     0.0000     2.1250     0.0000     2.2500     0.0000     0.0000
+--
 getFeaturesOrientation
   :: Features
   -> Array a
 getFeaturesOrientation = (`featuresToArray` af_get_features_orientation)
 
 -- | Get 'Feature' size
+--
+-- >>> getFeaturesSize (createFeatures 10)
+-- ArrayFire Array
+-- [10 1 1 1]
+--       nan        nan        nan        nan        nan        nan        nan        nan        nan        nan
+--
 getFeaturesSize
   :: Features
   -> Array a
--- test/ArrayFire/FeaturesSpec.hs
+++ test/ArrayFire/FeaturesSpec.hs
@@ -11,8 +11,3 @@ spec =
     it "Should get features number an array" $ do
       let feats = createFeatures 10
       getFeaturesNum feats `shouldBe` 10
-      -- let vec = vector @Double 10 $ repeat (acos 2)
-      -- getFeaturesSize feats `shouldBe` vec
-      -- getFeaturesOrientation feats `shouldBe` vec
-      -- getFeaturesXPos feats `shouldBe` vec
-      -- getFeaturesYPos feats `shouldBe` vec
