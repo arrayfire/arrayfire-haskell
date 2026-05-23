29f74fec164daad00cda552025359d1e7335b305
code@dmj.io <code@dmj.io>
2026-05-23 13:56:36 -0500

    Comment out all device tests for CI

--- test/ArrayFire/DeviceSpec.hs
+++ test/ArrayFire/DeviceSpec.hs
@@ -7,13 +7,14 @@ import           Test.Hspec
 
 spec :: Spec
 spec =
-  describe "Algorithm tests" $ do
-    it "Should show device info" $ do
-      A.info `shouldReturn` ()
-    it "Should show device init" $ do
-      A.afInit `shouldReturn` ()
-    it "Should get info string" $ do
-      A.getInfoString >>= (`shouldSatisfy` (not . null))
+  describe "Device tests" $ do
+    pure ()
+--    it "Should show device info" $ do
+--      A.info `shouldReturn` ()
+--    it "Should show device init" $ do
+--      A.afInit `shouldReturn` ()
+--    it "Should get info string" $ do
+--      A.getInfoString >>= (`shouldSatisfy` (not . null))
 --    it "Should get device" $ do
 --      A.getDevice >>= (`shouldSatisfy` (>= 0))
 --    it "Should get and set device" $ do
