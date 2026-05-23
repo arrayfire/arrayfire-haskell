bcdf649027b8ba8937a558490e9bee42eecab5e0
code@dmj.io <code@dmj.io>
2026-05-23 14:03:58 -0500

    Remove `setBackend`

--- test/Main.hs
+++ test/Main.hs
@@ -19,26 +19,9 @@ instance (A.AFType a, Arbitrary a) => Arbitrary (Array a) where
   arbitrary = pure $ unsafePerformIO (A.randu [2,2])
 
 main :: IO ()
-main = do
-  A.setBackend A.CPU
---  checks (Proxy :: Proxy (A.Array (A.Complex Float)))
---  checks (Proxy :: Proxy (A.Array (A.Complex Double)))
---  checks (Proxy :: Proxy (A.Array Double))
---  checks (Proxy :: Proxy (A.Array Float))
---  checks (Proxy :: Proxy (A.Array Double))
---  checks (Proxy :: Proxy (A.Array A.Int16))
---  checks (Proxy :: Proxy (A.Array A.Int32))
-  -- checks (Proxy :: Proxy (A.Array A.CBool))
-  -- checks (Proxy :: Proxy (A.Array Word))
-  -- checks (Proxy :: Proxy (A.Array A.Word8))
-  -- checks (Proxy :: Proxy (A.Array A.Word16))
-  -- checks (Proxy :: Proxy (A.Array A.Word32))
---  lawsCheck $ semigroupLaws (Proxy :: Proxy (A.Array Double))
---  lawsCheck $ semigroupLaws (Proxy :: Proxy (A.Array Float))
-  hspec spec
+main = hspec spec
 
 checks proxy = do
   lawsCheck (numLaws proxy)
   lawsCheck (eqLaws proxy)
   lawsCheck (ordLaws proxy)
---  lawsCheck (semigroupLaws proxy)
