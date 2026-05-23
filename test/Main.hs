50ce1169e9511c8746fa1b88b6825d214f0900d6
David Johnson <875324+dmjio@users.noreply.github.com>
2026-05-23 03:13:10 -0500

    Generalize `op2` type parameter. (#64)
    * Generalize `op2` type parameter.
    
    * Add CI
    
    * cabal install hspec-discover
    
    * hspec-discover
    
    * `cabal test`

--- test/Main.hs
+++ test/Main.hs
@@ -20,6 +20,7 @@ instance (A.AFType a, Arbitrary a) => Arbitrary (Array a) where
 
 main :: IO ()
 main = do
+  A.setBackend A.CPU
 --  checks (Proxy :: Proxy (A.Array (A.Complex Float)))
 --  checks (Proxy :: Proxy (A.Array (A.Complex Double)))
 --  checks (Proxy :: Proxy (A.Array Double))
