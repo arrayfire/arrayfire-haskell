8914928f5adde98ebbf1e07595586310086b93ba
David Johnson <875324+dmjio@users.noreply.github.com>
2026-06-05 13:21:38 -0500

    Add nix build to CI (#67)
    * Update nix build
    
    * `hspec-discover`
    
    * Use flakes
    
    * `autoPatchelfIgnoreMissingDeps = true;`
    
    * `cabal update`, `cabal install hspec-discover`
    
    * `export LD_LIBRARY_PATH` for ci.

--- flake.nix
+++ flake.nix
@@ -5,11 +5,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     nix-filter.url = "github:numtide/nix-filter";
-    arrayfire-nix = {
-      url = "github:twesterhout/arrayfire-nix";
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = inputs:
@@ -30,13 +25,50 @@
         ];
       };
 
+      # Build ArrayFire from the official binary installer; avoids freeimage entirely.
+      mkArrayfire = pkgs: pkgs.stdenv.mkDerivation rec {
+        pname = "arrayfire";
+        version = "3.10.0";
+        src = pkgs.fetchurl {
+          url = "https://arrayfire.s3.amazonaws.com/${version}/ArrayFire-v${version}_Linux_x86_64.sh";
+          hash = "sha256-8SibCWnRxts79S6WEHb3skF2TIDl1QnjY6EiohmoIog=";
+        };
+        nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+        # GPU/visualization libs (CUDA, OpenGL, GLFW, FreeImage, Intel OpenCL) are
+        # optional ArrayFire backends not available in headless CI environments.
+        autoPatchelfIgnoreMissingDeps = true;
+        buildInputs = with pkgs; [
+          stdenv.cc.cc.lib
+          fftw
+          fftwFloat
+          openblas
+          ocl-icd
+          boost.out
+        ];
+        unpackPhase = "true";
+        installPhase = ''
+          mkdir -p $out
+          bash $src --exclude-subdir --prefix=$out
+        '';
+        meta = {
+          description = "A general-purpose library for parallel and massively-parallel architectures";
+          platforms = [ "x86_64-linux" ];
+        };
+      };
+
+      arrayfire-overlay = self: super: {
+        arrayfire = mkArrayfire self;
+      };
+
       # An overlay that lets us test arrayfire-haskell with different GHC versions
       arrayfire-haskell-overlay = self: super: {
         haskell = super.haskell // {
           packageOverrides = inputs.nixpkgs.lib.composeExtensions super.haskell.packageOverrides
             (hself: hsuper: {
               arrayfire = self.haskell.lib.appendConfigureFlags
-                (hself.callCabal2nix "arrayfire" src { af = self.arrayfire; })
+                (hself.callCabal2nix "arrayfire" src {
+                  af = self.arrayfire;
+                })
                 [ "-f disable-default-paths" ];
             });
         };
@@ -55,21 +87,20 @@
             cabal-install
             doctest
             hsc2hs
-            hspec-discover
-            # Language servers
-            haskell-language-server
+            # hspec-discover
             nil
             # Formatters
             nixpkgs-fmt
           ];
           shellHook = ''
+            export LD_LIBRARY_PATH="${pkgs.arrayfire}/lib:$LD_LIBRARY_PATH"
           '';
         };
 
       pkgs-for = system: import inputs.nixpkgs {
         inherit system;
         overlays = [
-          inputs.arrayfire-nix.overlays.default
+          arrayfire-overlay
           arrayfire-haskell-overlay
         ];
       };
@@ -78,7 +109,6 @@
       packages = inputs.flake-utils.lib.eachDefaultSystemMap (system:
         with (pkgs-for system); {
           default = haskellPackages.arrayfire;
-          haskell = haskell.packages;
         });
 
       devShells = inputs.flake-utils.lib.eachDefaultSystemMap (system: {
